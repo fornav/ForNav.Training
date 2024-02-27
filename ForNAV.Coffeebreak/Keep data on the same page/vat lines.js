7ccd9db0bba260a66a8fb234e2d132d554670aca
René Brummel <rbrummel@redandbundle.com>
2024-02-27 10:56:39 +0100

    add same page coffeebreak

---
+++ ForNAV.Coffeebreak/Keep data on the same page/vat lines.js
@@ -0,0 +1,27 @@
+// VATAmountLine OnPreDataItem
+var vatLines = []
+
+// VATAmountLine OnAfterGetRecord
+var vatLine = {
+    vatIdentifier : VATAmountLine.VATIdentifier,
+    vatPct : VATAmountLine.VATPct,
+    lineAmount : VATAmountLine.LineAmount,
+    vatAmount : VATAmountLine.VATAmount,
+    vatBase : VATAmountLine.VATBase
+}
+
+vatLines.push(vatLine)
+
+// TextBoxes
+vatLines.map(vatLine => vatLine.vatIdentifier).join('\n')
+
+vatLines.map(vatLine => vatLine.vatPct).join('\n')
+
+vatLines.map(vatLine => CurrReport.DotNetFormat(vatLine.lineAmount, '#,0.00')).join('\n')
+vatLines.map(vatLine => vatLine.lineAmount).reduce((partialSum, a) => partialSum + a, 0)
+
+vatLines.map(vatLine => CurrReport.DotNetFormat(vatLine.vatAmount, '#,0.00')).join('\n')
+vatLines.map(vatLine => vatLine.vatAmount).reduce((partialSum, a) => partialSum + a, 0)
+
+vatLines.map(vatLine => CurrReport.DotNetFormat(vatLine.vatBase, '#,0.00')).join('\n')
+vatLines.map(vatLine => vatLine.vatBase).reduce((partialSum, a) => partialSum + a, 0)
